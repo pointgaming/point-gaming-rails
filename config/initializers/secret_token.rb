270ab0e49e1f0b015109b542b66bc4ee6a320306
Chris Bankston <chris@chrisbankston.com>
2013-01-21 15:07:50 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pointgaming::Application.config.secret_token = 'da7bccd590f9d9468be91737d9c1d7cceba1dadc3bb5262a17310289c6658efeeda1f2bd1be3802d783d62da546fc5dfd7b9c21935639db7b5a65a17b5d7be87'
