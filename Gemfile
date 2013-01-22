052e18181a92ea9958f27bf36b9e880cc04cabe8
Chris Bankston <chris@whitepaperclip.com>
2013-01-21 18:48:55 -0600

    Added twitter bootstrap and copied the layout from node

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
+gem 'twitter-bootstrap-rails'
+
 gem 'jquery-rails'
 
 # To use ActiveModel has_secure_password
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_current_path
+
+  private
+
+  def set_current_path
+    @current_path = request.fullpath
+  end
 end
