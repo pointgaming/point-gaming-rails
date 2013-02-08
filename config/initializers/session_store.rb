a6fcac9d0dc4143fed397f1181d0547e9ea0344f
Chris Bankston <chris@whitepaperclip.com>
2013-02-07 23:27:02 -0600

    Updated session to sync across all subdomains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Pointgaming::Application.config.session_store :redis_store
+Pointgaming::Application.config.session_store :redis_store, :key => '_pg_session', :domain => '.pointgaming.net'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
