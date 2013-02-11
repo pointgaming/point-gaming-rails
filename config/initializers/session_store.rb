fbe39a84cb4a80fe7a6200ea791213ebd4ac2878
Chris Bankston <chris@whitepaperclip.com>
2013-02-11 11:04:41 -0600

    Configured session_store per environment

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Pointgaming::Application.config.session_store :redis_store, :key => '_pg_session', :domain => '.pointgaming.net'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Pointgaming::Application.config.session_store :active_record_store
