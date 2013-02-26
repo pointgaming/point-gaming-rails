b72ba5a8354f162e3948b55c955c24c43ec2e89b
Chris Bankston <chris@whitepaperclip.com>
2013-02-26 14:46:24 -0600

    More API changes

--- app/models/friend_observer.rb
+++ app/models/friend_observer.rb
@@ -1,12 +1,12 @@
 class FriendObserver < Mongoid::Observer
   def after_create(record)
-    BunnyClient.instance.publish_fanout("u.#{record.user.username}", ::RablRails.render(record.friend_user, 'api/v1/friends/new'))
+    BunnyClient.instance.publish_fanout("u.#{record.user._id}", ::RablRails.render(record.friend_user, 'api/v1/friends/new'))
     if record.friend_user.status.eql?("online")
-      BunnyClient.instance.publish_fanout("u.#{record.user.username}", ::RablRails.render(record.friend_user, 'api/v1/friends/status_changed'))
+      BunnyClient.instance.publish_fanout("u.#{record.user._id}", ::RablRails.render(record.friend_user, 'api/v1/friends/status_changed'))
     end
   end
 
   def after_destroy(record)
-    BunnyClient.instance.publish_fanout("u.#{record.user.username}", ::RablRails.render(record.friend_user, 'api/v1/friends/destroy'))
+    BunnyClient.instance.publish_fanout("u.#{record.user._id}", ::RablRails.render(record.friend_user, 'api/v1/friends/destroy'))
   end
 end
--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -2,7 +2,7 @@ class UserObserver < Mongoid::Observer
   def after_update(record)
     if record.status_changed?
       record.friends.each do |friend|
-        BunnyClient.instance.publish_fanout("u.#{friend.friend_user.username}", ::RablRails.render(record, 'api/v1/friends/status_changed'))
+        BunnyClient.instance.publish_fanout("u.#{friend.friend_user._id}", ::RablRails.render(record, 'api/v1/friends/status_changed'))
       end
     end
   end
