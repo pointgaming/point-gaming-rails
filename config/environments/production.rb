ed9122cc6bda9465e8507ba8d76a0cf2b9a7c030
Chris Bankston <chris@whitepaperclip.com>
2013-03-01 15:00:34 -0600

    Improved rails asset compiling in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Pointgaming::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( *.js *.css )
+  config.assets.precompile += %w( profile.js stream.js team.js user_streams.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
