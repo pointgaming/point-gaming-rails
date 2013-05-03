3483b7923c0d82cf3e8be0dc294a2f5587e6d83a
Chris Bankston <chris@whitepaperclip.com>
2013-05-03 15:13:02 -0500

    Updated leaderboard on the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @news = News.desc(:created_at).all
     @tournaments = Tournament.all
+    @players = User.order_by(points: :desc).limit(5)
   end
 end
