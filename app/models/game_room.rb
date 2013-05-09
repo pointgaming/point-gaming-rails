2dce4e6115bbc528433f9eaeb1fdfee29d0c0956
Chris Bankston <chris@whitepaperclip.com>
2013-05-08 21:00:13 -0500

    Added ability to takeover another rooms position

--- app/models/game_room.rb
+++ app/models/game_room.rb
@@ -1,12 +1,17 @@
 class GameRoom
   include Mongoid::Document
 
+  # game room positions shouldn't shift after this limit
+  POSITION_MAX_SHIFT_THRESHOLD = 10
+
   scope :for, lambda {|game| where(game_id: game._id) }
 
   after_create :publish_created
   after_update :publish_updated
   after_destroy :publish_destroyed
   after_destroy :cancel_match
+
+  attr_accessor :takeover_position
 
   field :position, type: Integer
   field :is_advertising, type: Boolean, default: false
@@ -23,16 +28,66 @@ class GameRoom
 
   # has_and_belongs_to_many :team_bots
 
-  validates :position, presence: true, uniqueness: {scope: :game}, numericality: {only_integer: true, greater_than: 0, less_than: 1000}
+  validates :position, presence: true, numericality: {only_integer: true, greater_than: 0, less_than: 1000}
   validates :max_member_count, :presence=>true
 
+  validate :check_position_uniqueness
   validate :check_game_room_owner_count
 
   def mq_exchange
     "GameRoom_#{_id}"
   end
 
+  def self.next_available_position(game)
+    position = POSITION_MAX_SHIFT_THRESHOLD + 1
+    until !GameRoom.for(game).where(position: position).exists? do
+      position += 1
+    end
+    position
+  end
+
+  def move_to_next_position!
+    if position < POSITION_MAX_SHIFT_THRESHOLD
+      self.position += 1
+      next_room = GameRoom.for(game).where(position: position).nin(_id: _id).first
+      next_room.move_to_next_position! if next_room.present?
+      save
+    else
+      # move room to next available position
+      self.position = GameRoom.next_available_position(game)
+      save
+    end
+  end
+
 private
+
+  # FIXME: this needs to be implemented properly (check team/team bot points)
+  # returns true if the same users owns both game rooms
+  def can_takeover_room?(room_to_takeover)
+    owner_id === room_to_takeover.owner_id || owner.points > room_to_takeover.owner.points
+  end
+
+  def attempt_to_takeover_room(room_to_takeover)
+    unless can_takeover_room?(room_to_takeover)
+      self.errors[:base] << "Unable to takeover position."
+      return false
+    end
+
+    room_to_takeover.move_to_next_position!
+    true
+  end
+
+  # this method may call itself (recursive)
+  def check_position_uniqueness(takeover_attempted=false)
+    existing_room = GameRoom.for(game).where(position: self.position).nin(_id: self._id).first
+    return unless existing_room.present?
+    if takeover_position && takeover_attempted != true
+      self.takeover_position = false
+      check_position_uniqueness(true) if attempt_to_takeover_room(existing_room)
+    else
+      self.errors[:base] << (takeover_attempted ? "Failed to takeover position." : "Position has already been taken")
+    end
+  end
 
   def check_game_room_owner_count
     if GameRoom.where(game_id: self.game_id, owner_id: self.owner_id).nin(_id: self._id).exists?
