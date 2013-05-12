392e5e76f58e048da069f13b5deb15cf904901dc
Chris Bankston <chris@whitepaperclip.com>
2013-05-11 21:32:28 -0500

    Added bootstrap-sass gem and updated css files to use bootstrap-sass

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'sanitize'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
+  gem 'bootstrap-sass', '~> 2.3.1.0'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
 end
--- config/application.rb
+++ config/application.rb
@@ -65,5 +65,8 @@ module Pointgaming
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Required for rails 4
+    # config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
