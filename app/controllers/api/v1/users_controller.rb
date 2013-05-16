e3adb1cede8dc09a2c021b8145cd2dae17a86707
Chris Bankston <chris@whitepaperclip.com>
2013-05-16 02:22:12 -0500

    Updated the users api so you can lookup a user by id or slug

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::UsersController < Api::ApplicationController
   before_filter :authenticate_rails_app_api!
+  before_filter :ensure_user
 
   def show
-    @user = User.find_by(slug: params[:id])
     render json: {user: @user.as_json({ include: [:group], except: [:password] })}
   rescue
     render json: {}, status: 404
+  end
+
+  def ensure_user
+    if params[:slug].present?
+      @user = User.find_by(slug: params[:slug])
+    else
+      @user = User.find(params[:id])
+    end
   end
 
 end
