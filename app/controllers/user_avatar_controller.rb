10dcf37d949ae5214e45156121997e4799ccc629
Chris Bankston <chris@whitepaperclip.com>
2013-05-21 15:46:20 -0500

    Improved user avatar controller

--- app/controllers/user_avatar_controller.rb
+++ app/controllers/user_avatar_controller.rb
@@ -1,23 +1,33 @@
 class UserAvatarController < ApplicationController
   before_filter :authenticate_user!
   before_filter :ensure_user
+  before_filter :ensure_params, only: [:update]
   before_filter :ensure_user_id_is_current_user, only: [:edit, :update]
 
-  def edit
+  respond_to :html, :json
 
+  def edit
+    respond_with(@user)
   end
 
   def update
-    params[:user].delete("username")
-
-    if @user.update_attributes(params[:user])
-      redirect_to edit_user_profile_path(@user)
-    else
-      render :action => :edit
-    end
+    @user.update_attributes(update_params)
+    respond_with(@user, location: edit_user_profile_path(@user))
   end
 
 protected
+
+  def ensure_params
+    if params[:user].blank?
+      respond_with(nil, status: 422) do |format|
+        format.html { redirect_to :back, alert: 'Missing user parameter' }
+      end
+    end
+  end
+
+  def update_params
+    params[:user].slice(:avatar)
+  end
 
   def ensure_user
     @user = User.find_by slug: params[:user_id]
