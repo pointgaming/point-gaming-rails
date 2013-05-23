0585f6c99275e94f60a20ed93e2d6bb0b4cb3df3
Chris Bankston <chris@whitepaperclip.com>
2013-05-23 15:58:20 -0500

    Replaced tournaments with demos on the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @news = News.desc(:created_at).all
-    @tournaments = Tournament.all
+    @news = News.order_by(created_at: :desc).all
+    @demos = Demo.order_by(created_at: :desc).limit(5)
     @players = User.order_by(points: :desc).limit(5)
   end
 end
