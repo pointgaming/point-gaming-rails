79894e04891c1ca75a30e6d97ce486d3b6a5cc13
Chris Bankston <chris@whitepaperclip.com>
2013-07-05 19:53:03 -0500

    Moved streams_helper to core

--- app/helpers/streams_helper.rb
+++
@@ -1,9 +0,0 @@
-module StreamsHelper
-  def bet_tooltip(bet, user)
-    tags = []
-    [:risk_amount, :win_amount].each do |key|
-      tags << content_tag(:li, "#{Bet.human_attribute_name(key)}: #{bet.send(key, user)}")
-    end
-    content_tag :ul, tags.join
-  end
-end
