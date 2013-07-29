fb9e1ac8f1393be13b537a6454244042472e15e7
Travis Dunn <dunn.travis@gmail.com>
2013-07-29 22:56:51 +0200

    test show user api endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,6 +3,28 @@ require 'spec_helper'
 describe Api::V1::UsersController do
   render_views
 
+  describe '#show' do
+    let(:user) { Fabricate(:user) }
+    let(:request_params) { {api_token: node_api_token, format: :json, id: user._id} }
+
+    context 'with valid request_params' do
+      before(:each) do
+        get :show, request_params
+      end
+
+      it 'response status is 200' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'returns correct json' do
+        body = JSON.parse(response.body)
+        expect(body).to include('user')
+        user = body['user']
+        expect(user).to eq(JSON.parse(user.to_json))
+      end
+    end
+  end
+
   describe '#index' do
     let(:user1) { Fabricate(:user) }
     let(:user2) { Fabricate(:user) }
