1933ab9a88fe381d09f6963a9771613c7e0afb66
Chris Bankston <chris@whitepaperclip.com>
2013-09-15 22:41:47 -0500

    Fixed problems with match spec.

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -11,18 +11,6 @@ describe Match do
       match.is_team_vs_mode?.should be_true
     end
 
-    it "is true when there is at least one team" do
-      match.player_1 = Fabricate.build(:team)
-      match.player_2 = Fabricate.build(:user)
-      match.is_team_vs_mode?.should be_true
-    end
-
-    it "is true when there is at least one team" do
-      match.player_1 = Fabricate.build(:user)
-      match.player_2 = Fabricate.build(:team)
-      match.is_team_vs_mode?.should be_true
-    end
-
     it "is false when there are two users" do
       match.player_1 = Fabricate.build(:user)
       match.player_2 = Fabricate.build(:user)
@@ -46,35 +34,6 @@ describe Match do
       it 'is true for match.player_2' do
         match.can_be_disputed_by?(match.player_2).should be_true
       end
-    end
-
-    context 'when User vs Team match' do
-      let(:match) { Fabricate(:user_vs_team_match) }
-
-      it 'is false for some random user' do
-        user = Fabricate.build(:user)
-        match.can_be_disputed_by?(user).should be_false
-      end
-
-      it 'is true for match.player_1' do
-        match.can_be_disputed_by?(match.player_1).should be_true
-      end
-
-      it 'is false for match.player_2 team members' do
-        member = match.player_2.members.where(rank: 'Member').first
-        match.can_be_disputed_by?(member.user).should be_false
-      end
-
-      it 'is true for the match.player_2 team managers' do
-        manager = match.player_2.members.where(rank: 'Manager').first
-        match.can_be_disputed_by?(manager.user).should be_true
-      end
-
-      it 'is true for the match.player_2 team leader' do
-        leader = match.player_2.members.where(rank: 'Leader').first
-        match.can_be_disputed_by?(leader.user).should be_true
-      end
-
     end
 
     context 'when Team vs Team match' do
