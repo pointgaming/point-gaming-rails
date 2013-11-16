ae959742c72a47c32126fabab44a1046f215ed75
Chris Bankston <chris@whitepaperclip.com>
2013-11-16 16:59:40 -0600

    Removed tournaments from db/seeds. Cleaned up games in db/seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,29 +9,14 @@
 puts 'Setting up default games:'
 
 games = [
-  'Counter-Strike: Go',
-  'League of Legends',
-  'StarCraft II',
-  'Counter-Strike: Source',
-  'Quake 3',
-  'Quake Live'
+  'Quake Live',
+  'Counter-Strike (All Versions)'
 ]
 games.each {|game_name| 
   game = Game.create! :name => game_name, :player_count => 0
   puts 'New game created: ' << game.name
 }
 
-
-puts 'Creating Tournaments'
-
-game = Game.find_by(name: 'Quake Live')
-Tournament.create! :start_datetime => Time.now, :game => game, :game_type => 'Duel', :prize_pool => '$1,000 Cash/1000 Points', :player_limit => 5000, :player_count => 3492
-
-game = Game.find_by(name: 'Quake 3')
-Tournament.create! :start_datetime => Time.now, :game => game, :game_type => 'CTF', :prize_pool => '$1,000 Cash/1000 Points', :player_limit => 1000, :player_count => 459
-
-game = Game.find_by(name: 'Counter-Strike: Go')
-Tournament.create! :start_datetime => Time.now, :game => game, :game_type => 'Team 5vs5', :prize_pool => '$20,000 Cash/50,000 Points', :player_limit => 50000, :player_count => 12361
 
 groups = [
   {name: 'CEO/COO', prefix: '~'},
